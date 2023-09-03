dc1981c59c3716cafd15d804e686dc6dafb2a80f
Berk Unver <berkunver03@gmail.com>
2023-09-03 00:23:13 -0400

    Worked on fixing entity allocation leaks.

--- src/entity.zig
+++ src/entity.zig
@@ -27,6 +27,7 @@ pub const Entity = struct {
     position: math.Vector2 = math.vector2(0, 0),
     shear_x: math.Vector2 = math.vector2(1, 0),
     shear_y: math.Vector2 = math.vector2(0, 1),
+
     type_id: usize = undefined, // Used to make sure casting entities works properly. Is initialized when the entity is added to World.
     world: *World = undefined,
 
@@ -41,8 +42,8 @@ pub const EntityId = struct {
     generation: i32,
 };
 
-pub const EntityArrayEntry = struct {
-    entity: *Entity,
+pub const EntityEntry = struct {
+    entity: ?*Entity,
     generation: i32,
 };
 
@@ -56,28 +57,39 @@ pub fn is_entity(comptime T: type) bool {
 }
 
 pub const World = struct {
-    entities: std.ArrayList(EntityArrayEntry),
+    allocator: Allocator,
+    entities: std.ArrayList(EntityEntry),
     open_indicies: std.ArrayList(i32),
 
     pub fn init(allocator: Allocator) World {
         return World{
-            .entities = std.ArrayList(EntityArrayEntry).init(allocator),
+            .allocator = allocator,
+            .entities = std.ArrayList(EntityEntry).init(allocator),
             .open_indicies = std.ArrayList(i32).init(allocator),
         };
     }
 
     pub fn deinit(self: *World) void {
+        for (self.entities.items) |entry| {
+            std.debug.print("entity: {*}", .{entry.entity});
+            const e = entry.entity orelse continue;
+            if (e.vtable.deinit != null) e.vtable.deinit.?(e);
+            self.allocator.destroy(e);
+        }
         self.entities.deinit();
         self.open_indicies.deinit();
     }
 
-    pub fn entity_add(self: *World, comptime T: type, entity: *T) !EntityId {
+    pub fn entity_add(self: *World, comptime T: type, entity: T) !EntityId {
         comptime {
             if (!is_entity(T)) @compileError("E");
         }
 
-        var entity_base: *Entity = @ptrCast(entity);
-        entity_base.type_id = type_id(T);
+        var entity_ptr = try self.allocator.create(T);
+        entity_ptr.* = entity;
+
+        var e: *Entity = @ptrCast(entity_ptr);
+        e.type_id = type_id(T);
 
         // Match each function to one in the vtable if it exists and insert it.
         comptime var vtable = EntityVTable{};
@@ -111,28 +123,28 @@ pub const World = struct {
             }
         }
 
-        entity_base.vtable = vtable;
-        entity_base.world = self;
+        e.vtable = vtable;
+        e.world = self;
 
         if (self.open_indicies.items.len > 0) {
             const index: i32 = self.open_indicies.items[self.open_indicies.items.len - 1];
             self.open_indicies.items.len -= 1;
 
             const generation = self.entities.items[@intCast(index)].generation + 1;
-            self.entities.items[@intCast(index)] = EntityArrayEntry{
+            self.entities.items[@intCast(index)] = EntityEntry{
+                .entity = e,
                 .generation = generation,
-                .entity = entity_base,
             };
+
             return .{
                 .index = index,
                 .generation = generation,
             };
         } else {
-            const entry = EntityArrayEntry{
+            try self.entities.append(EntityEntry{
+                .entity = e,
                 .generation = 0,
-                .entity = entity_base,
-            };
-            try self.entities.append(entry);
+            });
             return .{
                 .index = @intCast(self.entities.items.len - 1),
                 .generation = 0,
@@ -140,16 +152,29 @@ pub const World = struct {
         }
     }
 
+    pub fn entity_deinit(self: *World, id: EntityId) void {
+        if (id.index < 0 or id.index >= self.entities.items.len) return;
+        const entry = self.entities.items[id.index];
+        const entity = entry.entity orelse return;
+        const func = entity.vtable.deinit orelse return;
+        if (func != null) func.?(entity);
+        self.allocator.free(entity);
+        self.entities.items[id.index].entity = null;
+    }
+
+    /// The pointer returned by this is stabilized.
     pub fn entity_get(self: *World, id: EntityId) ?*Entity {
-        if (self.entities.items.len <= id.index) return null;
-        if (self.entities.items[@intCast(id.index)].generation != id.generation) return null;
-        return self.entities.items[@intCast(id.index)].entity;
+        if (id.index < 0 or self.entities.items.len <= id.index) return null;
+        const e = self.entities.items[@intCast(id.index)];
+        if (e.generation != id.generation) return null;
+        return e.entity; // can be null.
     }
 
     pub fn update(self: *World, delta: f32) void {
         for (self.entities.items) |entry| {
-            const func = entry.entity.vtable.update orelse continue;
-            func(entry.entity, delta);
+            const entity = entry.entity orelse continue;
+            const func = entity.vtable.update orelse continue;
+            func(entity, delta);
         }
     }
 };
@@ -158,6 +183,14 @@ pub const Player = struct {
     e: Entity = .{},
     counter: i32 = 0,
     name: []const u8 = "Gamer",
+
+    const State = enum {
+        idle,
+        walk,
+        jump,
+        fall,
+        stun,
+    };
 
     pub fn update(player: *Player, delta: f32) void {
         _ = delta;
--- src/main.zig
+++ src/main.zig
@@ -1,58 +1,25 @@
 const std = @import("std");
 const print = std.debug.print;
 const Allocator = std.mem.Allocator;
-const game = @import("entity.zig");
+const e = @import("entity.zig");
 
 pub fn main() !void {
-    var GPA = std.heap.GeneralPurposeAllocator(.{}){};
-    var allocator = std.heap.page_allocator;
-    defer _ = GPA.deinit();
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer std.debug.assert(gpa.deinit() == .ok);
+    var allocator = gpa.allocator();
 
-    var world = game.World.init(allocator);
+    var world = e.World.init(allocator);
     defer world.deinit();
 
-    var player = try allocator.create(game.Player);
-    player.* = .{}; // Default initialization
-
-    var entity_id = try world.entity_add(game.Player, player);
+    var entity_id = try world.entity_add(e.Player, .{});
     var entity_retrieved = world.entity_get(entity_id) orelse {
         print("Could not retrieve the entity!\n", .{});
         return;
     };
     world.update(0);
-    var player_retrieved = entity_retrieved.cast(game.Player) orelse {
+    var player_retrieved = entity_retrieved.cast(e.Player) orelse {
         print("Entity retrieved is not a player!", .{});
         return;
     };
     print("player retrieved counter: {d}\n", .{player_retrieved.counter});
 }
-
-// pub fn Array(comptime T: type) type {
-//     return struct {
-//         const Self = @This();
-//
-//         items: ?*T,
-//         count: i32,
-//         count_allocated: i32,
-//         allocator: Allocator,
-//
-//         pub fn new(a: Allocator) Self {
-//             return .{
-//                 .items = null,
-//                 .count = 0,
-//                 .count_allocated = 0,
-//                 .allocator = a,
-//             };
-//         }
-//
-//         pub fn get(self: *Self, i: i32) T {
-//             if (i < 0 or i >= self.count) @panic("Out of bounds Array access");
-//             const items = self.items orelse @panic("Array members are uninitialized!");
-//             return items[i];
-//         }
-//
-//         pub fn set(self: *Self, i: i32, item: T) void {
-//             if (i < 0 or i >= self.count) @panic("Out of bounds Array access")
-//         }
-//     };
-// }
